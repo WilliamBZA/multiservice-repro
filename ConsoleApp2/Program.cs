4893282a64d11e2e509d8476d47b84a98a967d04
williambza <william.brander@gmail.com>
2022-04-21 08:50:32 +0200

    Add files

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Hosting;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using (var endpointOneBuilder = ConfigureEndpointOne(Host.CreateDefaultBuilder(args)).Build())
+        using (var endpointTwoBuilder = ConfigureEndpointTwo(Host.CreateDefaultBuilder(args)).Build())
+        {
+            await Task.WhenAll(endpointOneBuilder.StartAsync(), endpointTwoBuilder.StartAsync());
+            await Task.WhenAll(endpointOneBuilder.WaitForShutdownAsync(), endpointTwoBuilder.WaitForShutdownAsync());
+        }
+    }
+
+    static IHostBuilder ConfigureEndpointOne(IHostBuilder builder)
+    {
+        builder.UseWindowsService();
+
+        return builder;
+    }
+
+    static IHostBuilder ConfigureEndpointTwo(IHostBuilder builder)
+    {
+        builder.UseWindowsService();
+
+        return builder;
+    }
+}
